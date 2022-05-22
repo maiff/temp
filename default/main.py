d2a7ad8de177b637c028642fe7021192e2be9452
maiff <864306867@qq.com>
2022-05-22 22:09:41 +0800

    init

---
+++ default/main.py
@@ -0,0 +1,39 @@
+import os, datetime, json
+from functools import wraps
+from flask import Flask, redirect, request, jsonify, render_template, abort
+from google.cloud import storage
+from google.cloud import datastore
+from google.cloud import vision
+import logging
+
+from sympy import re 
+
+app = Flask(__name__)
+CLOUD_STORAGE_BUCKET = "final-project-123" 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Cat not found.", 404
+  
+@app.route("/api/health")
+def health():
+  return "OK", 200
+
+@app.route("/api/upload", methods=['POST'])
+def upload():
+
+  photo = request.files['file']
+
+  storage_client = storage.Client.from_service_account_json('final-project-123-350902-5facbeacff97.json')
+
+  bucket_name = CLOUD_STORAGE_BUCKET
+  bucket = storage_client.bucket(bucket_name)
+
+  blob = bucket.blob(photo.filename)
+  blob.upload_from_string(photo.read(), content_type=photo.content_type)
+  print(f"File uploaded: {photo.filename} to {blob.public_url}")
+
+  return blob.public_url
+
+if __name__ == '__main__':
+  app.run(debug=True, port=4000)
---
+++ tasks/main.py
@@ -0,0 +1,62 @@
+import os, datetime, json
+from flask import Flask, redirect, request, jsonify, render_template, abort
+from google.cloud import storage
+from google.cloud import datastore
+import logging
+from mailjet_rest import Client
+import os
+import requests
+
+url = "/api/health"
+
+api_key = '030561d0b18ee8414deb568c5cb08e85'
+api_secret = '1f754876829407ca3e6aecdfb9410e9c'
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+
+app = Flask(__name__)
+
+def send_email(email, content):
+  data = {
+    'Messages': [
+      {
+        "From": {
+          "Email": "xwt2101239@gmail.com",
+          "Name": "test"
+        },
+        "To": [
+          {
+            "Email": email,
+            "Name": "test"
+          }
+        ],
+        "Subject": content,
+        "TextPart": content,
+      }
+    ]
+  }
+  result = mailjet.send.create(data=data)
+  logging.info("email send code: "+str(result.status_code))
+  logging.info("email send rsp: "+str(result.json()))
+
+@app.route("/tasks/check_health")
+def check_health():
+  try:
+    rsp = requests.get(url)
+  except:
+    logging.info("check_health error")
+    send_email('xwt2101239@gmail.com',  "❌ Health Error - https://photo-timeline-shared.uc.r.appspot.com/")
+    return "ok", 200
+
+  if rsp.status_code == 200:
+    logging.info("check_health ok "+str(rsp.status_code))
+    send_email('xwt2101239@gmail.com',  "👍 Health Check - https://photo-timeline-shared.uc.r.appspot.com/")
+  else:
+    logging.info("check_health error "+str(rsp.status_code))
+    send_email('xwt2101239@gmail.com',  "❌ Health Error - https://photo-timeline-shared.uc.r.appspot.com/")
+
+  return "OK", 200
+  
+if __name__ == '__main__':
+  app.run(debug=True)
+
+
